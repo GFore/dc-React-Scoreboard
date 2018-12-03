b1fcb2529697ed6a12e138c9d5570b2f62aae217
Greg Foreman <gfore@mindspring.com>
2018-12-03 11:28:20 -0500

    increment score button added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {    //not really needed since it's top level, just here for practice
+    super(props);
+
+    //Set up state
+    this.state = {
+      scores: [
+        {id: 1, name: "bob",  score: 1001},
+        {id: 2, name: "sue",  score: 20},
+        {id: 3, name: "lou",  score: 500}
+      ]
+    }
+  }
   render() {
-    return (
+    return (  
       <div className="App">
+        <h1>Scoreboard App</h1>
+        <div className="score-container">
+          {this._scoresAsCards()}
+        </div>
       </div>
     );
   }
+
+  _scoresAsCards(){
+    const cards = this.state.scores.map(score => {
+      return (
+        <div key={score.id}>
+          <h2>Name: {score.name} || Score: {score.score}</h2>
+          <button onClick={() => this._incrementScoreById(score.id)}>+</button>
+        </div>
+      )
+    });
+    return cards;
+  }
+
+  //Version 1: use .map to manually construct replacement
+  _incrementScoreById(id){
+    // find the player in this.state.score
+    // increment their score
+    const newScores = this.state.scores.map(score => {
+      if(score.id !== id) {return score}
+      else {
+        return {
+          id: score.id,
+          name: score.name,
+          score: score.score+1
+        }
+      }
+    });
+    // call this.setState
+    this.setState({
+      scores: newScores
+    })
+  }
+
 }
 
 export default App;
